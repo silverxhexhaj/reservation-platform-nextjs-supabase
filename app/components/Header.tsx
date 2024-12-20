3d8ffec06781a28ecdedab167b6dc5a0cf827948
silverxhexhaj <xhexhajsilver@gmail.com>
2024-12-20 18:58:00 +0100

    fix header menu on mobile

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -158,9 +158,9 @@ export function Header({ user }: HeaderProps) {
   } as const;
 
   return (
-    <header className={`fixed top-0 left-0 right-0 z-50 transition-colors duration-300 will-change-height ${isScrolled ? 'bg-white h-20' : 'bg-transparent h-20'}`}>
-      <div className={`max-w-screen-2xl mx-auto px-8 flex justify-between items-center h-full transition-colors duration-300 ${isScrolled ? '' : ''}`}>
-        <Link href="/" className={`text-4xl font-semibold md:w-80 ${bebasNeue.className} ${isScrolled ? 'text-black' : 'text-white'}`}>
+    <header className={`fixed top-0 left-0 right-0 z-50 transition-colors duration-300 will-change-height ${isScrolled || isMenuOpen ? 'bg-white h-20' : 'bg-transparent h-20'}`}>
+      <div className={`max-w-screen-2xl mx-auto px-8 flex justify-between items-center h-full transition-colors duration-300 ${isScrolled || isMenuOpen ? '' : ''}`}>
+        <Link href="/" className={`text-4xl font-semibold md:w-80 ${bebasNeue.className} ${isScrolled || isMenuOpen ? 'text-black' : 'text-white'}`}>
           NOOOR
         </Link>
         
@@ -252,97 +252,101 @@ export function Header({ user }: HeaderProps) {
         </div>
 
         <button
-          className={`md:hidden focus:outline-none ${isScrolled ? 'text-gray-600' : 'text-white'}`}
+          className={`md:hidden focus:outline-none ${isScrolled || isMenuOpen ? 'text-gray-600' : 'text-white'}`}
           onClick={toggleMenu}
         >
           {isMenuOpen ? <X size={24} /> : <Menu size={24} />}
         </button>
       </div>
-      {isMenuOpen && (
-        <div className="md:hidden absolute top-20 left-0 right-0 bg-white shadow-md p-4 space-y-4">
+      <div 
+        className={`md:hidden absolute top-20 left-0 right-0 bg-white shadow-md px-8 py-4 space-y-4 transform transition-all duration-300 ease-in-out origin-top h-[calc(100vh-5rem)] flex flex-col justify-between ${
+          isMenuOpen 
+            ? 'opacity-100 translate-y-0 animate-slideDown' 
+            : 'opacity-0 -translate-y-4 pointer-events-none'
+        }`}
+      >
+        <div className="flex flex-col space-y-8 pb-8 justify-center items-center flex-1">
+          <Link href="/" className="text-2xl text-center text-gray-600 hover:text-gray-900">
+            Home
+          </Link>
+          <Link href="/pages/public/who-we-are" className="text-2xl text-center text-gray-600 hover:text-gray-900">
+            Who We Are?
+          </Link>
+          <Link href="/pages/public/pricing" className="text-2xl text-center text-gray-600 hover:text-gray-900">
+            Pricing
+          </Link>
+        </div>
+        {user ? (
           <div className="flex flex-col space-y-4">
-            <Link href="/" className="text-gray-600 hover:text-gray-900">
-              Home
-            </Link>
-            <Link href="/pages/public/who-we-are" className="text-gray-600 hover:text-gray-900">
-              Who We Are?
-            </Link>
-            <Link href="/pages/public/pricing" className="text-gray-600 hover:text-gray-900">
-              Pricing
-            </Link>
-          </div>
-          {user ? (
-            <div className="flex flex-col space-y-4">
-              <Link href="/pages/private/business/partner">
-                <Button className="w-full border border-gray-900 text-gray-900 hover:bg-black/10 font-semibold">
-                  For businesses
-                </Button>
-              </Link>
-              <DropdownMenu>
-                <DropdownMenuTrigger asChild>
-                  <Button variant="ghost" className="relative h-9 w-9 rounded-full">
-                    <Avatar className="h-9 w-9">
-                      <AvatarImage src="/avatars/01.png" alt={user.username} />
-                      <AvatarFallback className="bg-slate-100 text-slate-600">
-                        {user.username.slice(0, 2).toUpperCase()}
-                      </AvatarFallback>
-                    </Avatar>
-                  </Button>
-                </DropdownMenuTrigger>
-                <DropdownMenuContent className="w-56 bg-white" align="end" forceMount>
-                  <DropdownMenuLabel>
-                    <div className="flex flex-col space-y-1">
-                      <p className="text-sm font-medium">{user.username}</p>
-                      <p className="text-xs text-muted-foreground">john@example.com</p>
-                    </div>
-                  </DropdownMenuLabel>
-                  <DropdownMenuSeparator />
-                  <DropdownMenuGroup>
-                    <DropdownMenuItem>
-                      <User className="mr-2 h-4 w-4" />
-                      <span>Profile</span>
-                    </DropdownMenuItem>
-                    <DropdownMenuItem>
-                      <Settings className="mr-2 h-4 w-4" />
-                      <span>Settings</span>
-                    </DropdownMenuItem>
-                    <DropdownMenuItem>
-                      <HelpCircle className="mr-2 h-4 w-4" />
-                      <span>Help Center</span>
-                    </DropdownMenuItem>
-                  </DropdownMenuGroup>
-                  <DropdownMenuSeparator />
-                  <DropdownMenuItem className="text-red-600" onClick={handleSignOut}>
-                    <LogOut className="mr-2 h-4 w-4" />
-                    <span>Log out</span>
-                  </DropdownMenuItem>
-                </DropdownMenuContent>
-              </DropdownMenu>
-            </div>
-          ) : (
-            <div className="flex flex-col space-y-4">
-              <Button 
-                onClick={() => handleAuth('signin', '/pages/private/business/partner')}
-                className="w-full border border-gray-900 text-gray-900 hover:bg-black/10 font-semibold"
-              >
+            <Link href="/pages/private/business/partner">
+              <Button className="w-full border border-gray-900 text-gray-900 hover:bg-black/10 font-semibold">
                 For businesses
               </Button>
-              <Button 
-                onClick={() => handleAuth('signin')}
-                className="w-full bg-black text-white hover:bg-gray-800 uppercase"
-              >
-                Login
-              </Button>
-              <Button 
-                onClick={() => handleAuth('register')}
-                className="w-full bg-white text-black border border-black hover:bg-gray-100 uppercase"
-              >
-                Register
-              </Button>
-            </div>
-          )}
-        </div>
-      )}
+            </Link>
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild>
+                <Button variant="ghost" className="relative h-9 w-9 rounded-full">
+                  <Avatar className="h-9 w-9">
+                    <AvatarImage src="/avatars/01.png" alt={user.username} />
+                    <AvatarFallback className="bg-slate-100 text-slate-600">
+                      {user.username.slice(0, 2).toUpperCase()}
+                    </AvatarFallback>
+                  </Avatar>
+                </Button>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent className="w-56 bg-white" align="end" forceMount>
+                <DropdownMenuLabel>
+                  <div className="flex flex-col space-y-1">
+                    <p className="text-sm font-medium">{user.username}</p>
+                    <p className="text-xs text-muted-foreground">john@example.com</p>
+                  </div>
+                </DropdownMenuLabel>
+                <DropdownMenuSeparator />
+                <DropdownMenuGroup>
+                  <DropdownMenuItem>
+                    <User className="mr-2 h-4 w-4" />
+                    <span>Profile</span>
+                  </DropdownMenuItem>
+                  <DropdownMenuItem>
+                    <Settings className="mr-2 h-4 w-4" />
+                    <span>Settings</span>
+                  </DropdownMenuItem>
+                  <DropdownMenuItem>
+                    <HelpCircle className="mr-2 h-4 w-4" />
+                    <span>Help Center</span>
+                  </DropdownMenuItem>
+                </DropdownMenuGroup>
+                <DropdownMenuSeparator />
+                <DropdownMenuItem className="text-red-600" onClick={handleSignOut}>
+                  <LogOut className="mr-2 h-4 w-4" />
+                  <span>Log out</span>
+                </DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
+          </div>
+        ) : (
+          <div className="flex flex-col space-y-4">
+            <Button 
+              onClick={() => handleAuth('signin', '/pages/private/business/partner')}
+              className="w-full border border-gray-900 text-gray-900 hover:bg-black/10 font-semibold"
+            >
+              For businesses
+            </Button>
+            <Button 
+              onClick={() => handleAuth('signin')}
+              className="w-full bg-black text-white hover:bg-gray-800 uppercase"
+            >
+              Login
+            </Button>
+            <Button 
+              onClick={() => handleAuth('register')}
+              className="w-full bg-white text-black border border-black hover:bg-gray-100 uppercase"
+            >
+              Register
+            </Button>
+          </div>
+        )}
+      </div>
       <style jsx global>{`
         @keyframes emerge {
           0%, 100% {
@@ -352,6 +356,17 @@ export function Header({ user }: HeaderProps) {
           50% {
             transform: scaleX(2) translateX(0);
             opacity: 0.8;
+          }
+        }
+
+        @keyframes slideDown {
+          0% {
+            opacity: 0;
+            transform: translateY(-10px);
+          }
+          100% {
+            opacity: 1;
+            transform: translateY(0);
           }
         }
       `}</style>
