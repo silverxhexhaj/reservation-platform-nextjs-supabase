ef129a25abaa48ba35aa8586aaa4c2a3348e986d
silverxhexhaj <xhexhajsilver@gmail.com>
2025-02-17 22:09:26 +0100

    Enhance AnimatedGradient with refined color and animation duration

--- app/components/gradient/AnimatedGradient.tsx
+++ app/components/gradient/AnimatedGradient.tsx
@@ -5,12 +5,12 @@ import { motion } from "framer-motion";
 export const AnimatedGradient = () => {
   return (
     <motion.div
-      className="absolute inset-0 bg-gradient-to-r from-blue-800 to-pink-600"
+      className="absolute inset-0 bg-gradient-to-r from-blue-900 to-pink-700"
       animate={{
         backgroundPosition: ['0% 50%', '100% 50%', '0% 50%'],
       }}
       transition={{
-        duration: 10,
+        duration: 5,
         ease: "linear",
         repeat: Infinity,
       }}
